8b70f129618d97dea1fbecacb99579348479a524
Jonas Wirth <44641733+wijon@users.noreply.github.com>
2020-11-28 17:31:35 +0100

    Refactor PhoneBook Main

--- src/main/scala/PhoneBook/Main.scala
+++ src/main/scala/PhoneBook/Main.scala
@@ -5,22 +5,25 @@ import akka.pattern.ask
 import akka.util.Timeout
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
 import scala.io.StdIn
 import scala.language.postfixOps
+import scala.util.{Failure, Success}
+import scala.util.control.Breaks.{break, breakable}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val system = ActorSystem("demo-system")
     implicit val timeout: Timeout = Timeout(5 seconds)
 
-    val phoneBookTuttlingen = system.actorOf(Props(PhoneBookActor("C:\\Users\\weber_jon\\IdeaProjects\\ScalaActors\\src\\main\\scala\\PhoneBook\\Tuttlingen.csv")), "pbTut")
-    val phoneBookRottweil = system.actorOf(Props(PhoneBookActor("C:\\Users\\weber_jon\\IdeaProjects\\ScalaActors\\src\\main\\scala\\PhoneBook\\Rottweil.csv")), "pbRw")
-    val phoneBookKonstanz = system.actorOf(Props(PhoneBookActor("C:\\Users\\weber_jon\\IdeaProjects\\ScalaActors\\src\\main\\scala\\PhoneBook\\Konstanz.csv")), "pbKn")
+    val phoneBookTuttlingen = system.actorOf(Props(PhoneBookActor("src\\main\\scala\\PhoneBook\\Tuttlingen.csv")), "pbTut")
+    val phoneBookRottweil = system.actorOf(Props(PhoneBookActor("src\\main\\scala\\PhoneBook\\Rottweil.csv")), "pbRw")
+    val phoneBookKonstanz = system.actorOf(Props(PhoneBookActor("src\\main\\scala\\PhoneBook\\Konstanz.csv")), "pbKn")
 
     val phoneBookActors = Vector(phoneBookTuttlingen, phoneBookRottweil, phoneBookKonstanz)
 
-    System.out.println("Was möchten sie tun?")
+    System.out.println("Was möchten Sie tun?")
     System.out.println("(1) nach einem Vorname suchen")
     System.out.println("(2) nach einem Nachname suchen")
     System.out.println("(3) nach einer PLZ suchen")
@@ -28,20 +31,47 @@ object Main {
     System.out.println("(5) alle Daten durchsuchen")
 
     while (true) {
-      val input = StdIn.readLine()
-      val inputParts = input.split("[()]")
+      breakable {
+        val input = StdIn.readLine()
 
-      inputParts(1) match {
-        case "1" => phoneBookActors.foreach(actor => (actor ? SearchFirstname(inputParts(2))).andThen(
-          r => if (r.isSuccess) r.get.asInstanceOf[Vector[String]].foreach(println(_))))
-        case "2" => phoneBookActors.foreach(actor => (actor ? SearchLastname(inputParts(2))).andThen(
-          r => if (r.isSuccess) r.get.asInstanceOf[Vector[String]].foreach(println(_))))
-        case "3" => phoneBookActors.foreach(actor => (actor ? SearchPostcode(inputParts(2).toInt)).andThen(
-          r => if (r.isSuccess) r.get.asInstanceOf[Vector[String]].foreach(println(_))))
-        case "4" => phoneBookActors.foreach(actor => (actor ? SearchCity(inputParts(2))).andThen(
-          r => if (r.isSuccess) r.get.asInstanceOf[Vector[String]].foreach(println(_))))
-        case "5" => phoneBookActors.foreach(actor => (actor ? SearchAll(inputParts(2))).andThen(
-          r => if (r.isSuccess) r.get.asInstanceOf[Vector[String]].foreach(println(_))))
+        input match {
+          case "1" => println("Geben Sie einen Vornamen ein:")
+          case "2" => println("Geben Sie einen Nachnamen ein:")
+          case "3" => println("Geben Sie eine PLZ ein:")
+          case "4" => println("Geben Sie einen Ortsnamen ein:")
+          case "5" => println("Geben Sie ein Suchwort ein:")
+          case _ =>
+            println("Falsche Eingabe!")
+            break // Continue
+        }
+
+        val pattern = StdIn.readLine()
+        var messageType: SearchMessage = null
+
+        input match {
+          case "1" => messageType = SearchFirstname(pattern)
+          case "2" => messageType = SearchLastname(pattern)
+          case "3" => messageType = SearchPostcode(pattern.toInt)
+          case "4" => messageType = SearchCity(pattern)
+          case "5" => messageType = SearchAll(pattern)
+        }
+
+        phoneBookActors.foreach(actor => (actor ? messageType).andThen(
+          r => {
+            if (r.isSuccess)
+              r.get.asInstanceOf[Vector[String]].foreach(println(_))
+          }))
+
+        //        Await.result(Future.sequence(phoneBookActors.map(actor => actor ? messageType)), 10 seconds)
+        //          .foreach(results => )
+        //
+        //
+        //
+        //        Future.sequence(phoneBookActors.map(actor => actor ? messageType))
+        //          .andThen(result => result match {
+        //            case Success(value) => value.foreach(res => println(res))
+        //            case Failure(value) => println("Suche fehlgeschlagen")
+        //          })
       }
     }
   }
