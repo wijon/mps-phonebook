32bb9464d234a2938b042e3bf67634b1b6a6f7f5
Jonas Weber <jonas.weber@hermle.de>
2020-12-18 22:59:38 +0100

    Fix simple spark test

--- src/main/scala/TestSpark/Main.scala
+++ src/main/scala/TestSpark/Main.scala
@@ -6,9 +6,10 @@ object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
 
-    val logData = spark.read.textFile("./README.md").cache()
+    val logData = spark.read.textFile("src\\main\\scala\\TestSpark\\Text.txt").cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
+
     println(s"Lines with a: $numAs, Lines with b: $numBs")
 
     spark.stop()
