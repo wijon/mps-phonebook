21a5b9146d702db43c1f7f6676c10cf3fd0cffb1
Jonas Weber <jonas.weber@hermle.de>
2020-12-18 14:38:28 +0100

    Add Spark Packages

---
+++ src/main/scala/PhoneBookSpark/ResultConsumer.scala
@@ -0,0 +1,32 @@
+package PhoneBookSpark
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+
+import java.util
+import java.util.Properties
+import scala.jdk.CollectionConverters.IterableHasAsScala
+
+object ResultConsumer extends App {
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "something")
+
+  val consumer = new KafkaConsumer[String, String](props)
+  val TOPIC = "filter-result"
+
+  consumer.subscribe(util.Collections.singletonList(TOPIC))
+
+  println(s"Subscribed to topic: '$TOPIC'")
+  println("Start polling")
+  println()
+  println()
+
+  while (true) {
+    val records = consumer.poll(100)
+    for (record <- records.asScala) {
+      println(record.value())
+    }
+  }
+}
